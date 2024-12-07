027368aec1989aca171d69ae129d2e5d90ad3979
SaiLeelaOtikundala <123424591+SaiLeelaOtikundala@users.noreply.github.com>
2024-12-07 18:31:07 -0500

    model.py

--- model.py
+++ model.py
@@ -1,3 +1,8 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras import layers,models
+
 # Build the CNN model
 model = models.Sequential([
     layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
