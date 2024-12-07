739dcc163229291ec116e21c83394cb5ccaf0a1d
SaiLeelaOtikundala <123424591+SaiLeelaOtikundala@users.noreply.github.com>
2024-12-07 17:49:50 -0500

    utils.py

--- utils.py
+++ utils.py
@@ -11,3 +11,15 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
 import seaborn as sns
+
+def visualize(images, labels, num_images=10):
+    """
+    Visualizes a random subset of images with their corresponding labels.
+    
+    Arguments:
+    - images (numpy array or list): A 4D array containing images with shape (num_samples, height, width, channels).
+    - labels (numpy array or list): A 1D array containing the labels corresponding to the images.
+    - num_images (int): The number of images to display (default is 10).
+    
+    The function displays 'num_images' random images from the dataset along with their labels.
+    """
