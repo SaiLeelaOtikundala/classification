3626de7e399f78dc2539f7a05d1a2f9cdd1aafa0
SaiLeelaOtikundala <123424591+SaiLeelaOtikundala@users.noreply.github.com>
2024-12-10 16:18:04 -0500

    model.py

--- model.py
+++ model.py
@@ -24,6 +24,14 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 
 # Train the model
+x_train, x_test, y_train, y_test = train_test_split(images, label_data, test_size=0.2, random_state=42)
+
+x_train = np.transpose(x_train, (0, 2, 3, 1))  # Convert to (batch_size, 32, 32, 3)
+x_test = np.transpose(x_test, (0, 2, 3, 1))  # Convert to (batch_size, 32, 32, 3)
+
+y_train = to_categorical(y_train, num_classes=10)
+y_test = to_categorical(y_test, num_classes=10)
+
 history = model.fit(x_train, y_train, epochs=15, batch_size=64, validation_data=(x_test, y_test))
 
 
