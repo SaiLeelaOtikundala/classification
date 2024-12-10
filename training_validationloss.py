54bad61e716c56da79a5aec01c4296f76e11d688
SaiLeelaOtikundala <123424591+SaiLeelaOtikundala@users.noreply.github.com>
2024-12-10 16:45:56 -0500

    training_validationloss.py

--- training_validationloss.py
+++ training_validationloss.py
@@ -1,6 +1,40 @@
-import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras import layers,models
+from tensorflow import keras
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
+
+# Build the CNN model
+model = models.Sequential([
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(128, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(10, activation='softmax')  # 10 classes for CIFAR-10
+])
+# Summary of the model architecture
+model.summary()
+
+# Compile the model
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
 
 # Train the model
+x_train, x_test, y_train, y_test = train_test_split(images, label_data, test_size=0.2, random_state=42)
+
+x_train = np.transpose(x_train, (0, 2, 3, 1))  # Convert to (batch_size, 32, 32, 3)
+x_test = np.transpose(x_test, (0, 2, 3, 1))  # Convert to (batch_size, 32, 32, 3)
+
+y_train = to_categorical(y_train, num_classes=10)
+y_test = to_categorical(y_test, num_classes=10)
+
 history = model.fit(x_train, y_train, epochs=15, batch_size=64, validation_data=(x_test, y_test))
 
 # Plotting training & validation loss
